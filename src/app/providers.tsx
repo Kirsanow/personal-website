2a1f9887d373342a6fd2f936d302b8b950d46c93
Kirsanov <kirsnvartem@gmail.com>
2025-04-26 15:19:10 +0300

    update to password reset workflow

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -47,7 +47,7 @@ export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <AppContext.Provider value={{ previousPathname }}>
       <ThemeProvider
-        // forcedTheme="light"
+        forcedTheme="light"
         attribute="class"
         disableTransitionOnChange
       >
